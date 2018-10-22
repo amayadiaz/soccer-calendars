37880cf4c8fa7861e1facecd9a1169e55f3d7d0a
Julian Amaya <julian@Julians-MacBook-Pro-2.local>
2018-10-22 14:31:21 -0500

    Create new files, list teams and show calendars

---
+++ app.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+    $('#btnSearch').on('click', function(){
+         var teamSelectedId = $('#select-teams').val();
+         callTeams(teamSelectedId);  
+    });
+
+    $('#teams-list').on('click', '.btn-team', function(){
+        var teamId = $(this).attr('id');
+        var teamName = $(this).text();
+        callTeam(teamId, teamName);  
+    });
+
+});
+
+ function callTeams(teamSelectedId){
+    $('#teams-list').html('');
+     $.ajax({
+         url: "http://api.football-data.org/v2/competitions/"+teamSelectedId+"/teams",
+         type: "GET",
+         beforeSend: function(xhr){xhr.setRequestHeader('X-Auth-Token', '9273f2c112b641c997a94f444da1f51c');},
+         success: function(data) { 
+             
+             var teams = data.teams;
+             $.each(teams, function(index, team){
+                 $('#teams-list').append('<div class="col-md-3"><button id='+team.id+' type="button" class="btn btn-primary btn-team" data-toggle="modal" data-target="#exampleModal">'+team.name+'</button></div>');
+             });
+         }
+     });
+ }
+ 
+ function callTeam(teamId, teamName){
+     $.ajax({
+         url: "http://api.football-data.org//v2/teams/"+teamId+"/matches/",
+         type: "GET",
+         beforeSend: function(xhr){xhr.setRequestHeader('X-Auth-Token', '9273f2c112b641c997a94f444da1f51c');},
+         success: function(data) { 
+            var matches = data.matches;
+            $('#tbody').html('');
+            $('.modal-title').html('');
+            $('.modal-title').append(teamName + ' Schedule');
+            $.each(matches, function(index, match){
+                if (match.homeTeam.name == teamName) {
+                    $('#tbody').append('<tr><th>'+index+'</th><td>'+match.awayTeam.name+'</td><td>'+match.utcDate+'</td><td>'+match.status+'</td></tr>');
+                }else{
+                    $('#tbody').append('<tr><th>'+index+'</th><td>'+match.homeTeam.name+'</td><td>'+match.utcDate+'</td><td>'+match.status+'</td></tr>');
+                }
+            });
+             
+         }
+     });
+ }
+ 
+ 
+ 
+ 
